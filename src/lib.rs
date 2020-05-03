7ebe006840f5038e9ffae78f87f17ace14876100
Eric Berquist <eric.berquist@gmail.com>
2020-05-02 23:05:17 -0400

    Fixed a logic bug in base case for double factorial
        - let mut i = two;
        + let mut i = if *self % two == zero { two } else { one };
    
    * Implement signed (integer) factorial
    * Implement signed (integer) double factorial
    * Make GSL optional in float double factorial

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,10 @@
 extern crate approx;
 #[cfg(test)]
 extern crate num_bigint;
+#[cfg(feature = "gsl")]
+extern crate rgsl;
 
-use num_traits::{CheckedMul, Float, FloatConst, FromPrimitive, Unsigned};
+use num_traits::{CheckedMul, Float, FloatConst, FromPrimitive, Signed, Unsigned};
 
 /// Unary operator for computing the factorial of a number
 ///
@@ -41,10 +43,28 @@ pub trait Factorial<Target = Self> {
     }
 }
 
+pub trait SignedFactorial<Target = Self> {
+    fn checked_factorial(&self) -> Option<Target>;
+
+    fn factorial(&self) -> Target {
+        self.checked_factorial()
+            .expect("Overflow computing factorial")
+    }
+}
+
 /// Unary operator for computing the double factorial of a number
 ///
 /// Implements checked and unchecked versions of the formula
 pub trait DoubleFactorial<Target = Self> {
+    fn checked_double_factorial(&self) -> Option<Target>;
+
+    fn double_factorial(&self) -> Target {
+        self.checked_double_factorial()
+            .expect("Overflow computing double factorial")
+    }
+}
+
+pub trait SignedDoubleFactorial<Target = Self> {
     fn checked_double_factorial(&self) -> Option<Target>;
 
     fn double_factorial(&self) -> Target {
@@ -63,6 +83,23 @@ pub trait FloatDoubleFactorial<Target = Self> {
 }
 
 impl<T: PartialOrd + Unsigned + CheckedMul> Factorial<T> for T {
+    #[inline(always)]
+    fn checked_factorial(&self) -> Option<T> {
+        let mut acc = T::one();
+        let mut i = T::one() + T::one();
+        while i <= *self {
+            if let Some(acc_i) = acc.checked_mul(&i) {
+                acc = acc_i;
+                i = i + T::one();
+            } else {
+                return None;
+            }
+        }
+        Some(acc)
+    }
+}
+
+impl<T: PartialOrd + Signed + CheckedMul> SignedFactorial<T> for T {
     #[inline(always)]
     fn checked_factorial(&self) -> Option<T> {
         let mut acc = T::one();
@@ -98,28 +135,44 @@ impl<T: PartialOrd + Unsigned + CheckedMul + Copy> DoubleFactorial<T> for T {
     }
 }
 
+impl<T: PartialOrd + Signed + CheckedMul + Copy> SignedDoubleFactorial<T> for T {
+    #[inline(always)]
+    fn checked_double_factorial(&self) -> Option<T> {
+        let one = T::one();
+        let two = one + one;
+        let mut acc = one;
+        let mut i = if *self % two == T::zero() { two } else { one };
+        while i <= *self {
+            if let Some(acc_i) = acc.checked_mul(&i) {
+                acc = acc_i;
+                i = i + two;
+            } else {
+                return None;
+            }
+        }
+        Some(acc)
+    }
+}
+
 impl<T: Float + FloatConst + FromPrimitive> FloatDoubleFactorial<T> for T
 where
     f64: From<T>,
 {
     #[inline(always)]
     fn checked_double_factorial(&self) -> Option<T> {
-        let two = T::one() + T::one();
-        if *self < T::zero() {
+        let zero = T::zero();
+        let one = T::one();
+        let two = one + one;
+        if *self < zero {
             // The double factorial of a negative even argument is undefined
-            if *self % two == T::zero() {
-                return None;
+            if *self % two == zero {
+                None
+            } else {
+                Some(negative_odd_double_factorial(*self))
             }
-            // Calculate the answer directly.
-            let n = (*self + T::one()) / two;
-            Some(
-                two.powf(n)
-                    * T::from_f64(rgsl::gamma_beta::gamma::gamma(f64::from(n) + 0.5f64)).unwrap()
-                    / T::PI().powf(T::from_f32(0.5).unwrap()),
-            )
         } else {
             let mut acc = T::one();
-            let mut i = two;
+            let mut i = if *self % two == zero { two } else { one };
             while i <= *self {
                 acc = acc * i;
                 i = i + two;
@@ -127,6 +180,26 @@ where
             Some(acc)
         }
     }
+}
+
+#[cfg(feature = "gsl")]
+fn negative_odd_double_factorial<T: Float + FloatConst + FromPrimitive>(n: T) -> T
+where
+    f64: From<T>,
+{
+    let one = T::one();
+    let two = one + one;
+    let n = (n + one) / two;
+    two.powf(n) * T::from_f64(rgsl::gamma_beta::gamma::gamma(f64::from(n) + 0.5f64)).unwrap()
+        / T::PI().powf(T::from_f32(0.5).unwrap())
+}
+
+#[cfg(not(feature = "gsl"))]
+fn negative_odd_double_factorial<T: Float + FloatDoubleFactorial>(n: T) -> T {
+    assert!(n < T::zero());
+    let one = T::one();
+    let two = one + one;
+    n * (-one).powf((n - one) / two) / (-n).double_factorial()
 }
 
 #[cfg(test)]
@@ -137,16 +210,19 @@ mod tests {
     #[test]
     fn zero_fact_is_one() {
         assert_eq!(0u32.factorial(), 1u32);
+        assert_eq!(0i64.factorial(), 1i64);
     }
 
     #[test]
     fn one_fact_is_one() {
-        assert_eq!(1.factorial(), 1u32);
+        assert_eq!(1u32.factorial(), 1u32);
+        assert_eq!(1i64.factorial(), 1i64);
     }
 
     #[test]
     fn two_fact_is_two() {
-        assert_eq!(2.factorial(), 2u32);
+        assert_eq!(2u32.factorial(), 2u32);
+        assert_eq!(2i64.factorial(), 2i64);
     }
 
     #[test]
@@ -180,21 +256,51 @@ mod tests {
     #[test]
     fn zero_double_fact_is_one() {
         assert_eq!(0u32.double_factorial(), 1u32);
+        assert_eq!(0i32.double_factorial(), 1i32);
+        assert_abs_diff_eq!(0f32.double_factorial(), 1f32);
     }
 
     #[test]
-    fn one_double_fact_is_two() {
+    fn one_double_fact_is_one() {
         assert_eq!(1u32.double_factorial(), 1u32);
+        assert_eq!(1i32.double_factorial(), 1i32);
+        assert_abs_diff_eq!(1f32.double_factorial(), 1f32);
     }
 
     #[test]
     fn two_double_fact_is_two() {
         assert_eq!(2u32.double_factorial(), 2u32);
+        assert_eq!(1i32.double_factorial(), 1i32);
+        assert_abs_diff_eq!(2f32.double_factorial(), 2f32);
+    }
+
+    #[test]
+    fn three_double_fact_is_three() {
+        assert_eq!(3u32.double_factorial(), 3u32);
+        assert_eq!(3i32.double_factorial(), 3i32);
+        assert_abs_diff_eq!(3f32.double_factorial(), 3f32);
+    }
+
+    #[test]
+    fn four_double_fact_is_eight() {
+        assert_eq!(4u32.double_factorial(), 8u32);
+        assert_eq!(4i32.double_factorial(), 8i32);
+        assert_abs_diff_eq!(4f32.double_factorial(), 8f32);
+    }
+
+    #[test]
+    fn five_double_fact_is_fifteen() {
+        assert_eq!(5u32.double_factorial(), 15u32);
+        assert_eq!(5i32.double_factorial(), 15i32);
+        assert_eq!(5f32.double_factorial(), 15f32);
+        assert_eq!(5u64.double_factorial(), 15u64);
+        assert_eq!(5u128.double_factorial(), 15u128);
     }
 
     #[test]
     fn ten_double_fact() {
         assert_eq!(10u32.double_factorial(), 3840u32);
+        assert_abs_diff_eq!(10f32.double_factorial(), 3840f32);
     }
 
     #[test]
@@ -215,24 +321,39 @@ mod tests {
 
     #[test]
     fn negative_one_double_fact_is_one() {
+        assert_abs_diff_eq!(negative_odd_double_factorial(-1f32), 1f32);
+        assert_abs_diff_eq!(negative_odd_double_factorial(-1f64), 1f64);
         assert_abs_diff_eq!((-1f32).double_factorial(), 1f32);
         assert_abs_diff_eq!((-1f64).double_factorial(), 1f64);
     }
 
     #[test]
     fn negative_three_double_fact_is_negative_one() {
+        assert_abs_diff_eq!(negative_odd_double_factorial(-3f32), -1f32);
+        assert_abs_diff_eq!(
+            negative_odd_double_factorial(-3f64),
+            -1f64,
+            epsilon = 1.0e-15
+        );
         assert_abs_diff_eq!((-3f32).double_factorial(), -1f32);
         assert_abs_diff_eq!((-3f64).double_factorial(), -1f64, epsilon = 1.0e-15);
     }
 
     #[test]
     fn negative_five_double_fact_is_one_third() {
+        assert_abs_diff_eq!(negative_odd_double_factorial(-5f32), (1.0 / 3.0));
+        assert_abs_diff_eq!(
+            negative_odd_double_factorial(-5f64),
+            (1.0 / 3.0),
+            epsilon = 1.0e-15
+        );
         assert_abs_diff_eq!((-5f32).double_factorial(), (1.0 / 3.0));
         assert_abs_diff_eq!((-5f64).double_factorial(), (1.0 / 3.0), epsilon = 1.0e-15);
     }
 
     #[test]
     fn negative_nineteen_double_fact() {
+        assert_abs_diff_eq!(negative_odd_double_factorial(-19f32), (-1.0 / 34_459_425.0));
         assert_abs_diff_eq!((-19f32).double_factorial(), (-1.0 / 34_459_425.0));
     }
 
