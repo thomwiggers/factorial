7482cb43fd824c392aba4f140150f3c8a1cf7e1c
Thom Wiggers <thomwiggers@users.noreply.github.com>
2020-05-06 16:09:36 +0200

    Fix #3

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # Compute the factorial
 //!
 //! This crate provides some convenient and safe methods to compute the
-//! factorial and related functions in O(n) time.
+//! factorial and related functions the most naive way possible.
 //!
 //! They are not necessarily the fastest versions: there are prime sieve methods that
 //! compute the factorial in `O(n (log n loglog n)^2)`. Patches are welcome.
