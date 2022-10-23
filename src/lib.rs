3386929a51329170e59c34dd9db95ecbc39c510c
lemark <marcantoine_manningham.com>
2022-10-22 20:05:17 -0400

    Add prime swing algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,8 @@
 extern crate num_bigint;
 extern crate num_traits;
 
-use num_traits::{CheckedMul, Unsigned};
+use num_traits::{CheckedMul, FromPrimitive, ToPrimitive, Unsigned};
+use primal_sieve::Sieve;
 
 /// Unary operator for computing the factorial of a number
 ///
@@ -37,6 +38,17 @@ pub trait Factorial<Target = Self> {
         self.checked_factorial()
             .expect("Overflow computing factorial")
     }
+
+    fn prime_swing(n: Target, sieve: &Sieve) -> Target;
+
+    /// Returns `self!`, i.e. the factorial of `self` using the prime swing algorithm.
+    ///
+    /// # Examples
+    /// ```
+    /// use factorial::Factorial;
+    /// assert_eq!(10u32.factorial(), 3628800);
+    /// ```
+    fn psw_factorial(&self, sieve: &Sieve) -> Target;
 }
 
 /// Unary operator for computing the double factorial of a number
@@ -51,7 +63,16 @@ pub trait DoubleFactorial<Target = Self> {
     }
 }
 
-impl<T: PartialOrd + Unsigned + CheckedMul> Factorial<T> for T {
+impl<
+        T: PartialOrd
+            + Unsigned
+            + CheckedMul
+            + std::iter::Product
+            + Clone
+            + FromPrimitive
+            + ToPrimitive,
+    > Factorial<T> for T
+{
     #[inline(always)]
     fn checked_factorial(&self) -> Option<T> {
         let mut acc = T::one();
@@ -65,6 +86,36 @@ impl<T: PartialOrd + Unsigned + CheckedMul> Factorial<T> for T {
             }
         }
         Some(acc)
+    }
+
+    fn prime_swing(n: T, sieve: &Sieve) -> T {
+        let mut vec = vec![];
+        for prime in sieve
+            .primes_from(2)
+            .take_while(|x| *x <= n.clone().to_usize().unwrap())
+        {
+            let mut p = T::one();
+            let mut q = n.clone();
+            while q != T::zero() {
+                q = q / T::from_usize(prime).unwrap();
+                // q&1 == 1 if q is odd
+                if q.clone() % T::from_usize(2).unwrap() == T::one() {
+                    p = p * T::from_usize(prime).unwrap();
+                }
+            }
+            if p > T::one() {
+                vec.push(p);
+            }
+        }
+        vec.into_iter().product()
+    }
+
+    fn psw_factorial(&self, sieve: &Sieve) -> T {
+        if *self < T::from_usize(2).unwrap() {
+            return T::one();
+        }
+        let first_term = Self::psw_factorial(&(self.clone() / T::from_usize(2).unwrap()), sieve);
+        first_term.clone() * first_term.clone() * Self::prime_swing(self.clone(), sieve)
     }
 }
 
@@ -89,6 +140,8 @@ impl<T: PartialOrd + Unsigned + CheckedMul + Copy> DoubleFactorial<T> for T {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Instant;
+
     use super::*;
     use num_bigint::*;
 
@@ -133,6 +186,55 @@ mod tests {
             2u32.to_biguint().unwrap().checked_factorial(),
             Some(2u32.to_biguint().unwrap())
         );
+    }
+
+    #[test]
+    fn zero_psw_fact_is_one() {
+        let sieve = Sieve::new(1);
+        assert_eq!(0u32.psw_factorial(&sieve), 1u32);
+    }
+
+    #[test]
+    fn one_psw_fact_is_one() {
+        let sieve = Sieve::new(2);
+        assert_eq!(1.psw_factorial(&sieve), 1u32);
+    }
+
+    #[test]
+    fn two_psw_fact_is_two() {
+        let sieve = Sieve::new(3);
+        assert_eq!(2.psw_factorial(&sieve), 2u32);
+    }
+
+    #[test]
+    fn ten_psw_fact() {
+        let sieve = Sieve::new(10);
+        assert_eq!(10u32.psw_factorial(&sieve), 3_628_800);
+    }
+
+    #[test]
+    fn psw_biguint_support() {
+        let sieve = Sieve::new(2);
+        assert_eq!(
+            2u32.to_biguint().unwrap().psw_factorial(&sieve),
+            2u32.to_biguint().unwrap()
+        );
+    }
+
+    #[test]
+    fn psw_speed_test() {
+        let time_fac = Instant::now();
+        let fac = 1000_usize.to_biguint().unwrap().factorial();
+        let time_fac = time_fac.elapsed().as_micros();
+
+        let time_psw_fac = Instant::now();
+        let sieve = Sieve::new(1000);
+        let psw_fac = 1000_usize.to_biguint().unwrap().psw_factorial(&sieve);
+        let time_psw_fac = time_psw_fac.elapsed().as_micros();
+
+        assert_eq!(fac, psw_fac);
+        println!("{} > {}", time_fac, time_psw_fac);
+        assert!(time_fac > time_psw_fac);
     }
 
     #[test]
